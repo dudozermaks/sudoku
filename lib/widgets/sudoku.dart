9c52c2529671350f75a4d3cd5baeac8731c517b0
Maks <maks.razrushitel08@gmail.com>
2024-02-02 16:58:49 +0300

    Clean up code

--- lib/widgets/sudoku.dart
+++ lib/widgets/sudoku.dart
@@ -15,46 +15,10 @@ class SudokuWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return AspectRatio(
-    //   aspectRatio: 1 / 1,
-    //   child: LayoutGrid(
-    //     // Set the cols and rows to be equal sizes
-    //     columnSizes: List<TrackSize>.generate(9, (index) => 1.fr),
-    //     rowSizes: List<TrackSize>.generate(9, (index) => 1.fr),
-    //     children: [
-    //       for (var j = 0; j < 9; j++)
-    //         for (var i = 0; i < 9; i++) buildCell(Pos(i, j))
-    //     ],
-    //   ),
-    // );
-    // return Table(
-    //   children: [
-    //     for (var y = 0; y < 9; y++)
-    //       TableRow(
-    //         children: [
-    //           for (var x = 0; x < 9; x++)
-    // 				buildCell(Pos(x, y))
-    //             // FittedBox(
-    //             //   child: Container(
-    //             //     width: 20,
-    //             //     height: 20,
-    //             //     color: Colors.red,
-    //             //     child: Center(child: Text("9")),
-    //             //   ),
-    //             // )
-    //           // SizedBox.square(
-    //           // 	dimension: 10,
-    //           // 	// width: 10,
-    //           // 	// height: 10,
-    //           // 	// child: FittedBox(child: ColoredBox(color: Colors.red)),
-    //           // 	// color: Colors.red,
-    //           // )
-    //         ],
-    //       )
-    //   ],
-    // );
     var colorScheme = Theme.of(context).colorScheme;
+
     return HeroMode(
+      // If the field is saved, then turn on animation (for animating from saves screen)
       enabled: field.saveFileName != null,
       child: Hero(
         tag: field.saveFileName ?? "",
@@ -112,29 +76,26 @@ class SudokuWidget extends StatelessWidget {
   }
 
   Cell buildCell(Pos position) {
-		int index = position.toIndex();
+    int index = position.toIndex();
+
+    VoidCallback? onTap;
+    bool isSelected = false;
+    bool isError = false;
 
     if (setSelected != null) {
-			return Cell(
-				position: position,
-				onTap: () => setSelected!(position),
-				isSelected: position == field.selected,
-				isError: !field.isRightPlaced(index),
-				isUserPlaced: field.isUserPlaced(index),
-				clue: field.getClue(index),
-				pencilmarks: field.getPencilmarks(index),
-			);
+      onTap = setSelected!(position);
+      isSelected = position == field.selected;
+      isError = !field.isRightPlaced(index);
     }
-		else {
-			return Cell(
-				position: position,
-				onTap: null,
-				isSelected: false,
-				isError: false,
-				isUserPlaced: field.isUserPlaced(index),
-				clue: field.getClue(index),
-				pencilmarks: field.getPencilmarks(index),
-			);
-		}
+
+    return Cell(
+      position: position,
+      onTap: onTap,
+      isSelected: isSelected,
+      isError: isError,
+      isUserPlaced: field.isUserPlaced(index),
+      clue: field.getClue(index),
+      pencilmarks: field.getPencilmarks(index),
+    );
   }
 }
