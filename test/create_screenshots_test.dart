542f11903f477c55d3e8b8ef5226c8366f4d36d3
Maks <maks.razrushitel08@gmail.com>
2024-02-11 21:33:20 +0300

    make standalone function

--- test/create_screenshots_test.dart
+++ test/create_screenshots_test.dart
@@ -4,9 +4,26 @@ import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:sudoku/main.dart';
 import 'package:sudoku/other_logic/app_globals.dart';
 
+void makeScreenshot(Widget w, String testName, String fileName) {
+  testWidgets(testName, (WidgetTester tester) async {
+    tester.view.physicalSize = const Size(1080, 1920);
+
+    await tester.pumpWidget(w);
+
+    // Without this line the whole method does not work.
+    // main.png is just a blank screenshot.
+    await tester.pumpAndSettle();
+
+    await expectLater(
+      find.byType(SudokuApp),
+      matchesGoldenFile('screenshots/$fileName.png'),
+    );
+  });
+}
+
 void main() async {
-	// TODO: remove golden_toolkit as a dependecie (only needed for this one line)
-	await loadAppFonts();
+  // TODO: remove golden_toolkit as a dependecie (only needed for this one line)
+  await loadAppFonts();
   var appGlobals = AppGlobals();
   await appGlobals.loadForScreenshots();
 
@@ -14,18 +31,13 @@ void main() async {
   await appGlobals.themeBox.put("color", const Color(0xFF79E579));
 
   // TODO: make rust initialize (see integration test)
-  testWidgets('Home page', (WidgetTester tester) async {
-    tester.view.physicalSize = const Size(1080, 1920);
 
-    await tester.pumpWidget(SudokuApp(
+  makeScreenshot(
+    SudokuApp(
       appGlobals: appGlobals,
-    ));
-
-    // Without this line the whole method does not work.
-    // main.png is just a blank screenshot.
-    await tester.pumpAndSettle();
-
-    await expectLater(
-        find.byType(SudokuApp), matchesGoldenFile('screenshots/home_page.png'));
-  });
+			showDebugBanner: false,
+    ),
+    "Home page",
+    "home_page",
+  );
 }
